728f171d5973ad27401c815a3ea989d1d9e550ba
tit <EMPTY>
2025-05-12 21:47:22 +0300

    примерный список необходимых пакетов

---
+++ cmatrix_replica.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+// import (
+// 	"fmt"
+// 	"math/rand"
+// 	"os"
+// 	"time"
+// 	"golang.org/x/term"
+// )
+
+var symbols = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*")
+
+func main() {
+}
