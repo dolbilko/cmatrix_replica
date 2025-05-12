0e3ccf3c5a5df4c9d682bd604933bcd8ea119f1c
tit <EMPTY>
2025-05-12 23:15:28 +0300

    обновлённый .gitignore, проба управляющих escape кодов

--- cmatrix_replica.go
+++ cmatrix_replica.go
@@ -1,15 +1,17 @@
 package main
 
-import "fmt"
-// import (
-// 	"fmt"
-// 	"math/rand"
-// 	"os"
-// 	"time"
-// 	"golang.org/x/term"
-// )
+import (
+	"fmt"
+ 	"math/rand"
+	"time"
+)
+
 
 var symbols = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*")
 
 func main() {
+	fmt.Print("\u001b[2J")
+	fmt.Printf("\u001b[%d;%dH", 5, 7)
+	fmt.Print("а")
+	time.Sleep(time.Duration(5 * time.Second))
 }
--- go.mod
+++
@@ -1,8 +0,0 @@
-module cmatrix_replica
-
-go 1.24.0
-
-require (
-	golang.org/x/sys v0.33.0 // indirect
-	golang.org/x/term v0.32.0 // indirect
-)
