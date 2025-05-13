52fe737d0266290df33d5fe7054eaae1d6a39925
tit <EMPTY>
2025-05-13 17:23:33 +0300

    комментарии

--- cmatrix_replica.go
+++ cmatrix_replica.go
@@ -10,14 +10,14 @@ import (
 	
 
 func main() {
-	fmt.Print("\u001b[?25l")
-	ended := make(chan struct{})
+	fmt.Print("\u001b[?25l") // спрятать курсор
+	ended := make(chan struct{}) 
 	functions.Terminal_clear()
-	for x := 1; x < 101; x+=2 {
+	for x := 1; x < 101; x+=2 { // запуск горутин рендера "капель"
 		go functions.Drop_render(x, 40, ended)
 	}
 	go functions.Q_catching(ended)
-	<-ended
+	<-ended //ожидание закрытия канала чтобы его прочесть и идти дальше
 	functions.Terminal_clear()
-	fmt.Print("\u001b[0;0H\u001b[0m")
+	fmt.Print("\u001b[0;0H\u001b[0m") // вернуть курсор в 0;0, сбросить форматирование
 }
--- functions/functions.go
+++ functions/functions.go
@@ -1,3 +1,5 @@
+// файл с подключаемыми функциями для рендера эффекта матрицы
+
 package functions
 
 import (
@@ -12,26 +14,34 @@ var symbols = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345
 
 
 func random_char() rune {
+	// функция возвращающая случайную руну из среза выше
 	return symbols[rand.Intn(len(symbols))]
 }
 
 
 func draw_char(col, line int, char rune) {
+	// получает: col — номер столбца; line — номер строки; char — руну
+	// печатает: ANSI последовательность перемещения курсора и зелёного цвета, а после саму руну
 	fmt.Printf("\u001b[%d;%dH\u001b[2;32m%c", line, col, char)
 }
 
 
 func clear_char(col, line int) {
+	// получает: col — столбец; line — строку
+	// печатает пробел на этом месте
 	fmt.Printf("\u001b[%d;%dH ", line, col)
 }
 
 
 func Terminal_clear() {
+	// печатает ANSI последовательность очистки терминала
 	fmt.Print("\u001b[2J")
 }
 
 
 func Q_catching(channel chan struct{}) {
+	// получает channel — канал типа struct
+	// закрывает канал при обнаружении напечатанной буквы "q"
 	for {
 		char, _, err := keyboard.GetSingleKey()
 		if err != nil {
@@ -47,22 +57,24 @@ func Q_catching(channel chan struct{}) {
 
 
 func Drop_render(col, terminal_height int, ended <-chan struct{}){
-	time.Sleep(time.Duration(rand.Intn(100) + 100) * time.Millisecond)
+	// получает: col — номер столбца; terminal_height — высоту рабочей области; ended — канал типа struct{}
+	// в столбце с полученным номером последовательно вызывает функцию печати символа и пробела
+	// в результате получается "капля" определённой длины, скорость которой определяет задержка печати
+	time.Sleep(time.Duration(rand.Intn(500) + 100) * time.Millisecond) // задержка для неодновременного старта с другими горутинами
 	for {
 		select {
 		case <-ended:
 			return
 		default:
 			drop_length := rand.Intn(20)+5
-			speed := time.Duration(rand.Intn(50) + 25) * time.Millisecond
-			for line_head := 0; line_head < terminal_height + drop_length; line_head++ {
+			speed := time.Duration(rand.Intn(30) + 15) * time.Millisecond // задержка печати
+			for line_head := 0; line_head < terminal_height + drop_length; line_head++ { // цикл отрисовки "капли"
 				if line_head < terminal_height {
 					draw_char(col, line_head, random_char())
 				}
 				clear_char(col, line_head - drop_length)
 				time.Sleep(speed)
 			}
-			time.Sleep(time.Duration(rand.Intn(500)+10) * time.Millisecond)
 		}
 	}
 }
