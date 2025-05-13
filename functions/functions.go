c820e7c836760387c6d4c55fb1287f3862ae9897
tit <EMPTY>
2025-05-13 10:58:47 +0300

    убрал асинхронную функцию удаления хвоста, реализовал её же через for внутри горутины столбца

--- functions/functions.go
+++ functions/functions.go
@@ -4,31 +4,28 @@ import (
 	"fmt"
  	"math/rand"
 	"time"
+	// "sync"
 	"github.com/eiannone/keyboard"
 )
 
+
 var symbols = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*")
+
 
 func Random_char(symbols []rune) rune {
 	return symbols[rand.Intn(len(symbols))]
 }
 
+
 func Move_cursor(x, line int) {
 	fmt.Printf("\u001b[%d;%dH", line, x)
 }
+
 
 func Terminal_clear() {
 	fmt.Print("\u001b[2J")
 }
 
-
-func Tail_remover(x, line_start, line_finish, speed int) {
-	for line := line_start; line < line_finish; line++ {
-		Move_cursor(x, line)
-		fmt.Print(" ")
-		time.Sleep(time.Duration(speed) * time.Millisecond)
-	}
-}
 
 func Q_catching(channel chan struct{}) {
 	for {
@@ -44,6 +41,7 @@ func Q_catching(channel chan struct{}) {
 	}
 }
 
+
 func Drop_render(x, terminal_height int, ended <-chan struct{}){
 	for {
 		select {
@@ -51,19 +49,21 @@ func Drop_render(x, terminal_height int, ended <-chan struct{}){
 			return
 		default:
 			drop_length := rand.Intn(terminal_height/4)+3
-			tail := 0
 			speed := rand.Intn(100)+50
+			tail_start := -drop_length
 			for line := 0; line < terminal_height; line++ {
 				Move_cursor(x, line)
 				fmt.Printf("%c", Random_char(symbols))
-				tail = line-drop_length
-				if tail > 0 {
-					Move_cursor(x, tail)
+				if tail_start >= 0 && tail_start < terminal_height {
+					Move_cursor(x, tail_start)
 					fmt.Print(" ")
 				}
-				if terminal_height-line < 2 {
-					go Tail_remover(x, terminal_height-drop_length, terminal_height, speed)
-				}
+				tail_start++
+				time.Sleep(time.Duration(speed) * time.Millisecond)
+			}
+			for line := terminal_height - drop_length; line != terminal_height; line++ {
+				Move_cursor(x, line)
+				fmt.Print(" ")
 				time.Sleep(time.Duration(speed) * time.Millisecond)
 			}
 			time.Sleep(time.Duration(rand.Intn(50)+10) * time.Millisecond)
