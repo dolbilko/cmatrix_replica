a1692f995b23f50c4a7a220d60b8b72165766935
tit <EMPTY>
2025-05-19 14:47:32 +0300

    добавил defer для некоторых функций

--- cmatrix_replica.go
+++ cmatrix_replica.go
@@ -16,8 +16,8 @@ func main() {
 	for x := 1; x < 101; x+=2 { // запуск горутин рендера "капель"
 		go functions.Drop_render(x, 40, ended)
 	}
-	go functions.Q_catching(ended)
+	functions.Q_catching(ended)
 	<-ended //ожидание закрытия канала чтобы его прочесть и идти дальше
-	functions.Terminal_clear()
-	fmt.Print("\u001b[0;0H\u001b[0m") // вернуть курсор в 0;0, сбросить форматирование
+	defer functions.Terminal_clear()
+	defer fmt.Print("\u001b[0;0H\u001b[0m") // вернуть курсор в 0;0, сбросить форматирование
 }
